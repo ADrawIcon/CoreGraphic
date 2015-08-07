3b31f9db542d7405176db82df84e53b09787af1f
jobsyu <1175585627@qq.com>
2015-08-07 09:13:03 +0800

    Initial Commit

---
+++ CoreGraphics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreGraphics
+//
+//  Created by jobs on 15/8/7.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreGraphics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreGraphics
+//
+//  Created by jobs on 15/8/7.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
