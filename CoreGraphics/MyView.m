e903e0597bd52175723e08503c0748fa3e84558e
jobsyu <1175585627@qq.com>
2015-08-07 13:04:04 +0800

    qea

--- CoreGraphics/MyView.m
+++ CoreGraphics/MyView.m
@@ -11,11 +11,52 @@
 @implementation MyView
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+ Only override drawRect: if you perform custom drawing.
+ An empty implementation adversely affects performance during animation.*/
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    CGContextSetLineWidth(ctx, 10);
+    
+    CGContextMoveToPoint(ctx, 30, 30);
+    CGContextAddLineToPoint(ctx, 100, 100);
+    
+    CGContextStrokePath(ctx);
+    
+    
+//    NSString *str =  @"haha";
+//    [str drawInRect:CGRectMake(150, 150, 50, 50) withAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:13.f],NSForegroundColorAttributeName :[UIColor redColor]}];
+//    
+//    
+//    //渲染 瞄边
+//    CGContextStrokePath(ctx);
+//    CGContextSetStrokeColorWithColor(ctx, [[UIColor redColor] CGColor]);
+//    CGContextSetFillColorWithColor(ctx, [[UIColor blueColor] CGColor]);
+//    CGContextSetLineWidth(ctx, 20);
+//    
+//    //[[UIColor redColor] set];
+//    CGContextAddRect(ctx, CGRectMake(100, 100, 100 , 100));
+//    CGContextFillPath(ctx);
+//    CGContextStrokePath(ctx);
+    
+    //画圆
+    CGContextSetLineWidth(ctx, 5);
+    CGContextSetFillColorWithColor(ctx, [[UIColor redColor] CGColor]);
+    CGContextMoveToPoint(ctx, 200, 200);
+    CGContextAddArc(ctx, 200, 200, 100, 0, M_PI_2, 0);
+    //CGContextStrokePath(ctx);
+    CGContextFillPath(ctx);
+    
+    CGContextSetFillColorWithColor(ctx, [[UIColor blackColor] CGColor]);
+    CGContextMoveToPoint(ctx, 200, 200);
+    CGContextAddArc(ctx, 200, 200, 100, M_PI_2, M_PI, 0);
+    CGContextFillPath(ctx);
+    
+    CGContextSetFillColorWithColor(ctx, [[UIColor blueColor] CGColor]);
+    CGContextMoveToPoint(ctx, 200, 200);
+    CGContextAddArc(ctx, 200, 200, 100, M_PI, M_PI * 2, 0);
+    CGContextFillPath(ctx);
 }
-*/
+
 
 @end
--- CoreGraphics/ViewController.m
+++ CoreGraphics/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "MyView.h"
+#import "POP.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate>
 
 @end
 
@@ -16,12 +18,87 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    MyView *myView = [[MyView alloc] initWithFrame:self.view.bounds];
+    myView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:myView];
+    
+    //创建贝塞尔曲线
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(0, 400)];
+    
+    [path addLineToPoint:CGPointMake(100, 300)];
+    [path moveToPoint:CGPointMake(100, 300)];
+    
+    [path addLineToPoint:CGPointMake(200, 400)];
+    [path moveToPoint:CGPointMake(200, 400)];
+    [path addLineToPoint:CGPointMake(300, 500)];
+    [path moveToPoint:CGPointMake(300, 500)];
+    [path addLineToPoint:CGPointMake(400, 200)];
+    
+    //支付宝的账单动画(圆)
+    UIBezierPath *path1 = [UIBezierPath bezierPathWithArcCenter:CGPointMake(200, 400) radius:100 startAngle:0 endAngle:M_PI *2 clockwise:1];
+    
+    //颜色渐变
+    //CAGradientLayer
+    
+    //矩形
+    UIBezierPath *path2 = [UIBezierPath bezierPath];
+    [path2 moveToPoint:CGPointMake(40, 300)];
+    [path2 addLineToPoint:CGPointMake(40, 100)];
+    
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path1.CGPath;
+    shapeLayer.fillColor = [[UIColor blueColor] CGColor];
+    shapeLayer.lineWidth =5;
+    shapeLayer.strokeColor = [[UIColor redColor] CGColor];
+    [self.view.layer addSublayer:shapeLayer];
+    
+    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
+    anim.duration = 2;
+    anim.fromValue = @(1);
+    anim.toValue = @(0);
+    anim.autoreverses = NO;
+    anim.removedOnCompletion = NO;
+    [shapeLayer addAnimation:anim forKey:@"strokenStart"];
+    
+    
+    POPCustomAnimation *cus=[POPCustomAnimation animationWithBlock:^BOOL(id target, POPCustomAnimation *animation) {
+        CAShapeLayer *shapeLayer = target;
+        shapeLayer.strokeEnd += 0.01;
+        if (shapeLayer.strokeEnd >= 1) {
+            return NO;
+        } else {
+            return YES;
+        }
+        
+        
+    }];
+    shapeLayer.strokeEnd = 0;
+    [shapeLayer pop_addAnimation:cus forKey:@"ad"];
+    
+    //弹性动画
+    //damping指阻尼 值越小震动越大
+    //Velocity速度
+    UIView *view1 =[[UIView alloc] initWithFrame:CGRectMake(30, 400, 80, 80)];
+    [UIView animateWithDuration:2 delay:0 usingSpringWithDamping:0.3 initialSpringVelocity:0.6 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        view1.frame = CGRectMake(260, 200, 80, 80);
+    } completion:nil];
+    
+    
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    webView.delegate =self;
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"www.baidu.com"]]];
+    [self.view addSubview:webView];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    [webView stringByEvaluatingJavaScriptFromString:@"<script type='text/script'>"];
+    return YES;
 }
+
+
 
 @end
