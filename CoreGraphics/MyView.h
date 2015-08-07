c835c6af18ab54d25f4668aa6c45a78122e58c69
jobsyu <1175585627@qq.com>
2015-08-07 13:03:44 +0800

    quart2d

---
+++ CoreGraphics/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  CoreGraphics
+//
+//  Created by jobs on 15/8/7.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ CoreGraphics/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  CoreGraphics
+//
+//  Created by jobs on 15/8/7.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
